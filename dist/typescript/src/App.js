03030bd4b50b6610a9b0e1f21d0b4e90e2aedbe0
Aditya Srivastava <aditya123@gmail.com>
2026-07-05 15:37:23 +0530

    Stop tracking dist and node_modules

--- dist/typescript/src/App.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import './App.css';
-import './index.css';
-function App() {
-    return (_jsx("div", { children: "hello world" }));
-}
-export default App;
-//# sourceMappingURL=App.js.map
